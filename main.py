3e26a824d128bd258ec5852c7906b7e9bd5de06c
Yao-Cyriaque Koffi <yao-cyriaque.koffi@etu.univ-tlse2.fr>
2026-02-14 10:56:00 +0100

    railway

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ app = FastAPI(
 
 origins = [
     "http://localhost:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-    "*"
+    "https://howmuchia.netlify.app"
 ]
 
 app.add_middleware(
